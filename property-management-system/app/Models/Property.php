0b1beb4556d57a2deb5ab679592cdb7e2bc01f32
arunpragash <imarunpragash@gmail.com>
2025-02-22 13:22:15 +0530

    properties,tenants,users  model relationship created

--- property-management-system/app/Models/Property.php
+++ property-management-system/app/Models/Property.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Property extends Model
 {
-    use HasFactory;
+    use HasFactory , SoftDeletes;
+
+    protected $fillable = ['name','address','owner_id','rent_amount'];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class,'owner_id');
+    }
+
+    public function tenants()
+    {
+        return $this->hasMany(Tenant::class);
+    }
 }
--- property-management-system/app/Models/Tenant.php
+++ property-management-system/app/Models/Tenant.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tenant extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = ['name','email','phone_number','property_id','agreement_percentage'];
+
+    public function property()
+    {
+        return $this->belongsTo(Property::class);
+    }
 }
