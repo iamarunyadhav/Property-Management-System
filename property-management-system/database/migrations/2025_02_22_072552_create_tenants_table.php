7c9527bdfa292ac5edce158a52c4733f50781b52
arunpragash <imarunpragash@gmail.com>
2025-02-22 13:10:27 +0530

    properties,tenants migration and model created

---
+++ property-management-system/database/migrations/2025_02_22_072552_create_tenants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->foreignId('property_id')->constrained('properties')->onDelete('cascade');
+            $table->decimal('agreement_percentage', 5, 2)->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenants');
+    }
+};
