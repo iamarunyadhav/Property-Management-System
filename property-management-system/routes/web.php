d3898d9a5f12ef78aa3e9874d02e9b91dba5226e
arunpragash <imarunpragash@gmail.com>
2025-02-25 22:46:31 +0530

    API welcome blade disbaled in routes

--- property-management-system/routes/web.php
+++ property-management-system/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'Welcome to Property Management System API']);
 });
